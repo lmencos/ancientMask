6457d7a91784db945b7b7fdf17010dc7ffc1aa6b
lmencos <lmencos@hotmail.com>
2019-07-05 18:38:55 -0500

    Mask is ready

---
+++ src/components/Three.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+window.THREE = THREE;
+require('three/examples/js/loaders/GLTFLoader');
+require('three/examples/js/controls/OrbitControls');
+
+export default window.THREE;
