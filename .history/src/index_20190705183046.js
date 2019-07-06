7870655dc2e5c41b15c86ebafbdde46ede9d15d0
lmencos <lmencos@hotmail.com>
2019-07-05 19:09:37 -0500

    Mask ready

--- .history/src/index_20190705183046.js
+++ .history/src/index_20190705183046.js
@@ -3,22 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter, Route, Link, Switch } from 'react-router-dom';
 
-
-
-ReactDOM.render(
-  
-  
-  <HashRouter>
-    <Switch>
-      <Route exact path="/" component = {App} />
-    </Switch>
-
-  </HashRouter>,
-
-
-document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
