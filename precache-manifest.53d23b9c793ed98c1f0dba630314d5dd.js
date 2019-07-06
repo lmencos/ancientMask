6575908ec3c7e588e11511efc538d1265285458b
lmencos <lmencos@hotmail.com>
2019-07-05 19:10:55 -0500

    Updates

---
+++ precache-manifest.53d23b9c793ed98c1f0dba630314d5dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b3bfbc22c65961241c4187b222fbd0",
+    "url": "/ancientMask/index.html"
+  },
+  {
+    "revision": "8b59066a639354ed9363",
+    "url": "/ancientMask/static/css/main.d65ecdb2.chunk.css"
+  },
+  {
+    "revision": "20f096936bbf3f9531a6",
+    "url": "/ancientMask/static/js/2.6693dcaa.chunk.js"
+  },
+  {
+    "revision": "8b59066a639354ed9363",
+    "url": "/ancientMask/static/js/main.ef8749f7.chunk.js"
+  },
+  {
+    "revision": "c90c4bda75504e59e2b5",
+    "url": "/ancientMask/static/js/runtime~main.3d8bd661.js"
+  }
+]);
